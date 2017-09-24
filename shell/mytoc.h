0391e30a89419d8b904b62ec36a5f4e789a79028
Stefany Carrillo <scarrillo7@miners.utep.edu>
2017-09-23 22:25:11 -0600

    commit header file

---
+++ shell/mytoc.h
@@ -0,0 +1,25 @@
+#ifndef mytoc_included
+#define mytoc_included
+
+//demo method
+int main();
+
+char **myToc(char *str, char delim);
+
+char *copyWord(char *str, int length, char delim);
+
+int countLetters(char *str, char delim);
+
+char *startWord(char *str, char delim);
+
+int countWords(char *str, char delim);
+
+void printVector(char **vector);
+
+void freeVector(char **vector);
+
+void readInput();
+
+int programOff(char *input);
+
+#endif
