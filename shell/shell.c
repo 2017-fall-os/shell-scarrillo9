90d60944001c99e7a15911661c6feac94317cb45
Stefany Carrillo <scarrillo7@miners.utep.edu>
2017-09-24 18:03:57 -0600

    append added

--- shell/shell.c
+++ shell/shell.c
@@ -1,53 +1,55 @@
 #include <sys/types.h>		/* for lseek, read */
-#include <unistd.h>		/* for lseek, read */
+#include <unistd.h>		    /* for lseek, read */
 #include <sys/stat.h>		/* for read */
-#include <fcntl.h>		/* for read */
-#include <stdlib.h>		/* for malloc */
+#include <fcntl.h>		    /* for read */
+#include <stdlib.h>		    /* for malloc */
 #include <stdio.h>
+#include <sys/wait.h>
+#include <string.h>
 
 #include "mytoc.h" 
-
+#define ff fflush(stdout);
 #define BUFSIZE 256
 
-int main(){
-	readInput();
-	
-	//char **tokens = myToc("   hey what's your   name", ' ');
-	
-	//freeVector(tokens);
+
+int main(int argc, char **argv, char **envp){
+    int rc = fork();
+    
+    if(rc < 0){
+        write(0, "fork failed", 11);
+        exit(1);
+    }//end if fork failed
+    
+    else if(rc == 0){
+        printf("I am child (pid:%d)\n", (int)getpid()); ff;
+        
+        char *cmd = "ls";
+        int pathNum = getPath(envp);
+        char *path = startWord(envp[pathNum], '='); path++;
+        //printf("path: %s\n", path); ff;
+        char **pathVector = myToc(path, ':');
+        char **temp = pathVector;
+        for(; temp; temp++){
+            char *tempExe = append(*temp, cmd);
+            printf("%s\n", tempExe); ff;
+            //int retVal = execve(tempExe, cmd);
+            //fprintf(stderr, "%s: exec returned %d\n", argv[0], retVal);
+        }//end while
+    }//end else if fork
+    
+    else{
+        int wc = wait(NULL);
+        printf("I am parent of %d (wc:%d) (pid:%d)\n", rc, wc, (int)getpid()); ff;
+    }//end else
     return 0;
 }//end main
 
-int programOff(char *input){
-    char *exit = "exit";
-    if(input[0]==exit[0] && input[1]==exit[1] && input[2]==exit[2] && input[3]==exit[3]){
-			return 1;
-    }//end if exit
-    return 0;
-}//end program off
+int getPath(char **envp){
+    int i =0;
+    for(i = 0; envp[i] != (char*)0; i++){
+        if(envp[i][0] == 'P' && envp[i][1] == 'A' && envp[i][2] == 'T' && envp[i][3] == 'H')
+            break;
+    }//end for loop
+    return i;
+}//end getPath
 
-void readInput(){
-	int on = 1;
-    //char *input[BUFSIZE];
-    
-	while(on){
-        char *input[BUFSIZE]= {0};
-		write(1, "$ ", 2);
-		read(0, input, sizeof input);
-        
-        for(int i = 0; input[i] != 0; i++){
-            if(input[i] == '\n')
-                input[i] = '\0';
-        }//end for
-		
-		if(programOff(input)){
-			on = 0; break;
-		}//end if exit
-		
-		char **tokens = myToc(input, ' ');
-		
-		//char **tokens = myToc("   hey what's your   name", ' ');
-		//freeVector(tokens);
-        write(1, "after free", 10);
-	}//end while on
-}//end readInput
